59c57e7ea44f022b7469b2aa67bce5a5f23ac95b
Jamie Cruwys <6881571+JamieCruwys@users.noreply.github.com>
2022-07-15 10:33:25 +0100

    Setup App Startup initializer for Timber and added Timber to modules (#14)

---
+++ app/src/main/java/uk/co/jamiecruwys/showcase/TimberInitializer.kt
@@ -0,0 +1,15 @@
+package uk.co.jamiecruwys.showcase
+
+import android.content.Context
+import androidx.startup.Initializer
+import timber.log.Timber
+
+class TimberInitializer : Initializer<Unit> {
+    override fun create(context: Context) {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+    override fun dependencies(): List<Class<out Initializer<*>>> = listOf()
+}
--- ui/src/main/java/uk/co/jamiecruwys/showcase/ui/screen/DogsScreen.kt
+++ ui/src/main/java/uk/co/jamiecruwys/showcase/ui/screen/DogsScreen.kt
@@ -3,9 +3,11 @@ package uk.co.jamiecruwys.showcase.ui.screen
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import timber.log.Timber
 
 @Composable
 fun dogsScreen() {
+    Timber.d("Showing dog screen")
     Box {
         Text(text = "Woof!")
     }
