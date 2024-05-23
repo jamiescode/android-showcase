9ba24ac686eb033a93f0316c908cd385fb285902
Jamie Cruwys <6881571+JamieCruwys@users.noreply.github.com>
2024-05-23 14:04:27 +0100

    Project upgrade (#97)
    * Upgraded from JDK 11 to 17
    * Upgraded android from 7.2.2 to 8.4.1
    * Upgraded gradle from 7.6.4 to 8.7

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -21,10 +21,10 @@ repositories {
 
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
-    jvmTarget = "11"
+    jvmTarget = "17"
 }
 
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
-    jvmTarget = "11"
+    jvmTarget = "17"
 }
