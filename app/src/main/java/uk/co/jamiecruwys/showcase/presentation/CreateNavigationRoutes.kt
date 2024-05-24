db32737e5f1c3db69c3eccd2a6e5380557737bd2
Jamie Cruwys <6881571+JamieCruwys@users.noreply.github.com>
2024-05-24 22:32:41 +0100

    Removed navController

--- app/src/main/java/uk/co/jamiecruwys/showcase/presentation/CreateNavigationRoutes.kt
+++ app/src/main/java/uk/co/jamiecruwys/showcase/presentation/CreateNavigationRoutes.kt
@@ -1,13 +1,12 @@
 package uk.co.jamiecruwys.showcase.presentation
 
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import uk.co.jamiecruwys.dog.presentation.screen.dogsScreen
 import uk.co.jamiecruwys.gratitude.presentation.screen.gratitudeScreen
 import uk.co.jamiecruwys.navigation.Destinations
 
-fun NavGraphBuilder.createNavigationRoutes(navController: NavController) {
+fun NavGraphBuilder.createNavigationRoutes() {
     composable(
         route = Destinations.Dogs.route,
     ) {
