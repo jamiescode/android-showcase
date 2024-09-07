ec39cee2015400c89e6b9bca31d04d0158decced
Jamie <6881571+jamiescode@users.noreply.github.com>
2024-09-07 16:30:56 +0100

    Fix lint

--- feature_gratitude/src/main/java/com/jamiescode/showcase/gratitude/presentation/screen/composable/GratitudeListState.kt
+++ feature_gratitude/src/main/java/com/jamiescode/showcase/gratitude/presentation/screen/composable/GratitudeListState.kt
@@ -13,7 +13,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.jamiescode.showcase.gratitude.R
