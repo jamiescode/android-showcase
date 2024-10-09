66fd4dde55eb1004dcb937c4baa1e4db06ce7d3a
Jamie <6881571+jamiescode@users.noreply.github.com>
2024-10-09 17:58:20 +0100

    Set background for content to white

---
+++ theme/src/main/java/com/jamiescode/showcase/theme/Theme.kt
@@ -0,0 +1,31 @@
+package com.jamiescode.showcase.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val lightThemeColors = lightColorScheme(
+    background = Color.White,
+    surfaceContainer = Color.White,
+    surface = Color.White,
+)
+
+private val darkThemeColors = darkColorScheme()
+
+private val typography = Typography()
+
+@Composable
+fun ShowcaseTheme(
+    content: @Composable () -> Unit,
+) {
+    val colorScheme = if (isSystemInDarkTheme()) darkThemeColors else lightThemeColors
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = typography,
+        content = content,
+    )
+}
